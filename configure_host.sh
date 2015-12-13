41075d780fdbe3c7559968fc6faa6bf925e5ca6d
pat <pat@22acacia.com>
2015-12-12 20:42:51 -0800

    move to all build prep steps to a script
    this was proving too much a hassle to leave it to circleci to
    try and deal with configs

---
+++ configure_host.sh
@@ -0,0 +1,14 @@
+echo "download and install terraform and custom provider"
+curl https://releases.hashicorp.com/terraform/0.6.8/terraform_0.6.8_linux_amd64.zip -o ~/terraform.zip
+unzip terraform.zip -d /usr/local/bin/
+curl https://s3-us-west-1.amazonaws.com/22acacia-deploy/custom-binaries/terraform-provider-googlecli -o /usr/local/bin/terraform-provider-googlecli
+chmod +x /usr/local/bin/terraform-provider-googlecli
+
+echo "ensure kubectl is installed and that dataflow commands for gcloud are installed"
+which kubectl
+if [[ $? -eq 1 ]]; then
+  /opt/google-cloud-sdk/bin/gcloud components install kubectl -q
+fi
+/opt/google-cloud-sdk/bin/gcloud alpha -h < /bin/echo
+chown ubuntu:ubuntu -R ~/.config/
+
